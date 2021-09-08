a6044e5cbe5f4ce82ff7f4689c02e45ec2629eb2
katicabogar87 <katicabogar87@gmail.com>
2021-09-08 19:54:12 +0200

    mysql script

---
+++ src/main/resources/pirate_db.sql
@@ -0,0 +1,32 @@
+DROP DATABASE  IF EXISTS pirate_db;
+CREATE DATABASE IF NOT EXISTS pirate_db;
+
+USE pirate_db;
+
+DROP TABLE  IF EXISTS pirate;
+CREATE TABLE pirate(
+id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+name VARCHAR(50),
+strength  INT UNSIGNED  NOT NULL,
+health  INT UNSIGNED  NOT NULL, 
+/* is_alive BOOL; ( = health =? 0) */
+drunk_level ENUM('sober','tipsy','brave','drunk','wasted'),
+ship_id  INT UNSIGNED NOT NULL
+);
+
+DROP TABLE  IF EXISTS captain;
+CREATE TABLE captain(
+id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+rum_owned INT UNSIGNED NOT NULL
+);
+
+DROP TABLE  IF EXISTS ship;
+CREATE TABLE ship(
+id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+name VARCHAR(50),
+graphic_id INT,
+number_of_cannons INT UNSIGNED  NOT NULL,
+ship_condition  INT UNSIGNED  NOT NULL
+);
+
+
